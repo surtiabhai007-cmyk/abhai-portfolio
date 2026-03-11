cd7fdf80c81d52d121d68410d904513f6e559a66
surtiabhai007-cmyk <surti.abhai007@gmail.com>
2026-03-12 02:10:59 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-background/80 backdrop-blur border-b border-white/10 z-50">
+    <header className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur border-b border-white/10 z-50">
       <div className="max-w-6xl mx-auto flex items-center justify-between px-6 py-4">
 
         {/* Logo / Name */}
