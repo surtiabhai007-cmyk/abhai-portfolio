4ea073c4820ac8c85d9f96e7c88af0baa5c956f9
surtiabhai007-cmyk <surti.abhai007@gmail.com>
2026-03-12 18:17:42 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 py-8 mt-16">
+    <footer className="border-t border-gray-200 py-8 mt-12">
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row items-center justify-between text-sm text-gray-600">
         <p>
           © {new Date().getFullYear()} Abhai Surti. All rights reserved.
